3867f79be2c67aab6730695f16ddb41baaa1db99
Christian B <christianbear1221@gmail.com>
2019-04-17 18:17:49 -0700

    working working

---
+++ userRequestsData.js
@@ -0,0 +1,14 @@
+
+// functions that list up and out array lists of requests for a particular user
+
+function pullInivites(myEmail){
+    // function to that pulls all invite data
+}
+
+async function sendInvite(from, to, chatroomSelected){
+    // function that sends a Chat Room invite from me to them
+}
+
+async function confirmInvite(inviteArray, myEmail){
+    // function that confirms Chat Room invite from an array list of invites, attached to this user
+}
