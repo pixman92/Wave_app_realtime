b0bd67bc3d7e984ffe5aa540ae0cf827afa6a649
ChristianB <gotrustd@verizon.net>
2019-07-19 16:40:51 -0700

    working on counter for msg(s)

---
+++ creatingMsg.js
@@ -0,0 +1,12 @@
+// file that creates message based on chatroom
+
+function addMessage(roomID, msg, senderEmail, counter){
+ 
+    // db.collection("/chatrooms/I4IwkTcAnzFWUhs5dKHL").update()
+
+    addDataMergeTrue("/chatrooms/I4IwkTcAnzFWUhs5dKHL/messages", {message: msg, email: senderEmail, counter: counter});
+
+    
+
+
+}
